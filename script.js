f161e41bf9dbece2b7315bf32a24172ee1f91610
Francesco Pandolfi <francesco.pan@gmail.com>
2019-12-18 15:43:28 +0100

    Programma completato

--- script.js
+++ script.js
@@ -8,20 +8,29 @@ var passengerAge = prompt("L'età del passeggero?");
 // Definisco il prezzo per 1 km
 var price1Km = 0.21;
 
-// Definisco il prezzo totale da 18 ai 65 anni
+// Definisco il prezzo totale
 var totPrice = numKm * 0.21;
+
 
 // Condizione per determinare se ha diritto ad uno sconto
 if (passengerAge < 18) {
   // Se è minorenne lo sconto è del 20%
-  var totPriceUnder18 = (totPrice - (totPrice * 0.2)).toFixed(2);
+  totPrice = (totPrice - (totPrice * 0.2)).toFixed(2);
 } else if (passengerAge >= 65){
   // Se è over 65 compreso lo sconto è del 40%
-  var totPriceOver65 = (totPrice - (totPrice * 0.4)).toFixed(2);
+  totPrice = (totPrice - (totPrice * 0.4)).toFixed(2);
+} else {
+  // Altrimenti nessuno sconto
 }
+console.log("prezzo totale = " + totPrice);
 
-console.log(totPrice);
-console.log(totPriceUnder18);
+
+
+// document.getElementById('price').innerHTML = "10";
+// document.getElementById('price').innerHTML = totPriceUnder18;
+
+// document.getElementById('numofkm').innerHTML = numKm;
+// document.getElementById('agepass').innerHTML = passengerAge;
 
 // Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio.
 
